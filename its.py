5630a4f9fcaa01002192dba42200bc7bc8f50558
Catofes <i.catofes.com>
2015-03-17 13:48:01 +0800

    update

--- its.py
+++ its.py
@@ -1,7 +1,6 @@
 #!/bin/python2
+# -*- coding: UTF-8 -*-
 
-#import mechanize
-import cookielib
 import re
 import urllib2
 import urllib
@@ -11,6 +10,7 @@ import threading
 import falcon
 import json
 import systemd.journal
+import cPickle
 from wsgiref import simple_server
 
 
@@ -49,7 +49,46 @@ def web_login():
     br.submit()
 """
 
+
 class ITS:
+    class AccountManager:
+
+        class AccountInfo:
+            def __init__(self, name, password, type=0):
+                self.name = name
+                self.password = password
+                self.last_connect_time = 0
+                self.connect_limit_reach = False
+                self.disconnect_count = 0
+                self.type = type  # 0 for unlimited account. Should connect first. 1 for normal account. 2 for disable
+
+        def __init__(self):
+            self.date = time.strftime('%Y-%m-%d', time.localtime(time.time()))
+            self.accounts = [
+                self.AccountInfo('gwxpwjz', 'lijiao214', 0),
+                self.AccountInfo('1100011354', 'Sqrt21414', 1)
+            ]
+
+        def get_account(self):
+            date = time.strftime('%Y-%m-%d', time.localtime(time.time()))
+            if date != self.date:
+                for account in self.accounts:
+                    account.connect_limit_reach = False
+                self.date = date
+            # search for type 0
+            for account in self.accounts:
+                if account.type == 0:
+                    if account.connect_limit_reach:
+                        continue
+                    return account
+            # search for type 1
+            for account in self.accounts:
+                if account.type == 1:
+                    if account.connect_limit_reach:
+                        continue
+                    return account
+            return self.accounts[0]
+
     def __init__(self):
         self.last_request_time = 0
         self.last_check_time = 0
@@ -60,50 +99,78 @@ class ITS:
         self.lost_limit = 1
         self.check_url = ['http://plumz.me/generate_204',
                           'http://ipv4.i.catofes.com/']
+        self.account_manager = self.AccountManager()
         self.lock = threading.Lock()
 
     def c_lock(func):
-        def acquire_lock(self, op):
+        def acquire_lock(self, account=None):
             if self.lock.acquire(1):
-                result = func(self, op)
+                result = func(self, account)
                 self.lock.release()
                 return result
             else:
                 return False
+
         return acquire_lock
 
     @c_lock
-    def connect(self, op):
-        if op == 'connect':
-            range = '2'
-            operation = 'connect'
-        else:
-            range = '4'
-            operation = 'disconnectall'
-        if op == 'connect':
-            self.last_request_time = time.time()
+    def connect(self, account):
+        if not account:
+            account = self.account_manager.get_account()
         try:
             resp = urllib2.urlopen(
                 "https://its.pku.edu.cn:5428/ipgatewayofpku",
                 urllib.urlencode(
                     {
-                        "uid": "gwxpwjz",
-                        "password": "lijiao214",
-                        "range": range,
-                        "operation": operation,
+                        "uid": account.name,
+                        "password": account.password,
+                        "range": '1',
+                        "operation": 'connect',
                         "timeout": "1"
                     }
-                ),timeout=5
+                ), timeout=5
             )
         except Exception as e:
             self.last_request_result = False
-            systemd.journal.send(time.strftime('%Y-%m-%d  %H:%M:%S',time.localtime(self.last_check_time)) +
-                  " Reconnect Request Sent Error.")
+            systemd.journal.send(time.strftime('%Y-%m-%d  %H:%M:%S  ', time.localtime(self.last_check_time)) +
+                                 account.name + "   Connect Request Sent Error.")
             return False
-        self.last_request_result = True
         self.last_request_response = resp.read().decode('GB2312')
-        systemd.journal.send(time.strftime('%Y-%m-%d  %H:%M:%S',time.localtime(self.last_check_time)) +
-                  " Reconnect Request Sent Without Error.")
+        if re.search(ur'今天不能再使用客户端', self.last_request_response, re.UNICODE):
+            account.connect_limit_reach = True
+            systemd.journal.send(time.strftime('%Y-%m-%d  %H:%M:%S  ', time.localtime(self.last_check_time)) +
+                                 account.name + "   Reach Daily API Limit.")
+            self.last_check_result = False
+            return False
+        if re.search(ur'当前连接数超过预定值', self.last_request_response, re.UNICODE):
+            self.disconnect(account)
+            self.last_check_result = False
+            return False
+        self.last_check_result = True
+        systemd.journal.send(time.strftime('%Y-%m-%d  %H:%M:%S  ', time.localtime(self.last_check_time)) +
+                             account.name + "   Connect Request Sent Without Error.")
+        return True
+
+    def disconnect(self, account):
+        try:
+            resp = urllib2.urlopen(
+                "https://its.pku.edu.cn:5428/ipgatewayofpku",
+                urllib.urlencode(
+                    {
+                        "uid": account.name,
+                        "password": account.password,
+                        "range": '4',
+                        "operation": 'disconnectall',
+                        "timeout": "1"
+                    }
+                ), timeout=5
+            )
+        except Exception as e:
+            systemd.journal.send(time.strftime('%Y-%m-%d  %H:%M:%S  ', time.localtime(self.last_check_time)) +
+                                 account.name + "   Disconnectall Request Sent Error.")
+            return False
+        systemd.journal.send(time.strftime('%Y-%m-%d  %H:%M:%S  ', time.localtime(self.last_check_time)) +
+                             account.name + "   Disconnectall Request Sent Without Error.")
         return True
 
     def check(self):
@@ -121,8 +188,8 @@ class ITS:
             break
         self.last_check_result = last_check_result
         if not self.last_check_result:
-            systemd.journal.send(time.strftime('%Y-%m-%d  %H:%M:%S',time.localtime(self.last_check_time)) +
-                  " Detect Connection Lost. Try to reconnect.")
+            systemd.journal.send(time.strftime('%Y-%m-%d  %H:%M:%S  ', time.localtime(self.last_check_time)) +
+                                 "Detect Connection Lost. Try to reconnect.")
             return False
         return True
 
@@ -130,12 +197,14 @@ class ITS:
         self.lost_count += 1
         if self.lost_count >= self.lost_limit:
             if self.lost_limit >= 64:
-                self.lost_limit = math.floor(256*math.sqrt(self.lost_count/256.0))
+                self.lost_limit = math.floor(256 * math.sqrt(self.lost_count / 256.0))
             else:
                 self.lost_limit *= 2
-            if self.lost_limit >= 8:
-                self.connect('disconnect')
-            self.connect('connect')
+            if self.lost_limit < 1:
+                self.lost_limit = 1
+            if self.lost_count >= 256:
+                self.lost_limit = 256
+            self.connect()
             return True
         return False
 
@@ -170,8 +239,8 @@ class WebService:
         resp.status = falcon.HTTP_200
         resp.body = json.dumps({
             'check_status': self.its.last_check_result,
-            'check_time': time.strftime('%Y-%m-%d  %H:%M:%S',time.localtime(self.its.last_check_time)),
-            'request_time': time.strftime('%Y-%m-%d  %H:%M:%S',time.localtime(self.its.last_request_time)),
+            'check_time': time.strftime('%Y-%m-%d  %H:%M:%S', time.localtime(self.its.last_check_time)),
+            'request_time': time.strftime('%Y-%m-%d  %H:%M:%S', time.localtime(self.its.last_request_time)),
             'request_response': self.its.last_request_response,
             'next_reconnect_time': time.strftime('%Y-%m-%d  %H:%M:%S',
                                                  time.localtime((self.its.lost_limit - self.its.lost_count) * 5
@@ -186,14 +255,19 @@ class WebService:
             resp.status = falcon.HTTP_400
         resp.body = json.dumps({
             'check_status': self.its.last_check_result,
-            'check_time': time.strftime('%Y-%m-%d  %H:%M:%S',time.localtime(self.its.last_check_time)),
-            'request_time': time.strftime('%Y-%m-%d  %H:%M:%S',time.localtime(self.its.last_request_time)),
+            'check_time': time.strftime('%Y-%m-%d  %H:%M:%S', time.localtime(self.its.last_check_time)),
+            'request_time': time.strftime('%Y-%m-%d  %H:%M:%S', time.localtime(self.its.last_request_time)),
             'request_response': self.its.last_request_response,
             'next_reconnect_time': time.strftime('%Y-%m-%d  %H:%M:%S',
                                                  time.localtime((self.its.lost_limit - self.its.lost_count) * 5
                                                                 + time.time()))
         })
         return True
+
+    def on_options(self, req, resp):
+        resp.status = falcon.HTTP_200
+        resp.body = json.dumps([self.its.lost_count, self.its.lost_limit,
+                                list(name.__dict__ for name in self.its.account_manager.accounts)])
 
 its = ITS()
 app = falcon.API()
